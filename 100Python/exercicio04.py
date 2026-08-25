e73b74b4e56dd5fe5fb2912dfc43bc249c6085fb
Odair Gabriel <32102507+OdaGab@users.noreply.github.com>
2026-08-25 15:15:00 -0300

    Exercicio 04

---
+++ 100Python/exercicio04.py
@@ -0,0 +1,7 @@
+numero = int(input("Digite um número:"))
+
+numeroD = 2 * numero
+numeroT = 3 * numero
+numeroM = numero / 2
+
+print(f"O número coletado foi {numero}, o Dobro é {numeroD}, o Triplo é {numeroT}, e a metade é {numeroM}")
