9faa05506ed450d5a5f75947a4b44dcfb866feef
Odair Gabriel <32102507+OdaGab@users.noreply.github.com>
2026-08-29 23:37:58 -0300

    exercicio16.py

---
+++ 100Python/exercicio16.py
@@ -0,0 +1,13 @@
+num = int(input("Digite um número: "))
+
+print(f"numero digitado", num)
+
+if(num > 0):
+    print("POSITIVO")
+
+elif(num < 0):
+    print("NEGATIVO")
+else:
+    print("O Número digitado deve ser diferente de 0")
+  
+
